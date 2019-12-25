e9181aee6008731623dceea2bf9db262cc47c5d0
Marcin.Nowak <marcin.nowak@techmetria.pl>
2019-12-25 14:09:31 +0100

    Tests for lexer

--- CogPar/src/test/java/co/uk/cogitolearning/cogpar/parser/ParserTest.java
+++ CogPar/src/test/java/co/uk/cogitolearning/cogpar/parser/ParserTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ParserTest {
 
     @Test
-    public void shouldParse() {
+    public void shouldParseAdd2() {
         // Given
         Parser parser = new Parser();
 
@@ -32,6 +32,33 @@ public class ParserTest {
                 node,
                 new ConstantNode(1.0),
                 new ConstantNode(2.0));
+    }
+
+    @Test
+    public void shouldParseAdd3() {
+        // Given
+        Parser parser = new Parser();
+
+        // When
+        ExpressionNode tree = parser.parse(new LinkedList<>(Arrays.asList(
+                new Token(7, "1", 0),
+                new Token(1, "+", 1),
+                new Token(7, "2", 2),
+                new Token(1, "+", 3),
+                new Token(7, "3", 4)
+        )));
+        // Then
+        AdditionNode node;
+        node = new AdditionNode(new ConstantNode(1.0), true);
+        node.add(new ConstantNode(2.0), true);
+        node.add(new ConstantNode(3.0), true);
+
+        assertThat(tree).containsExactly(
+                node,
+                new ConstantNode(1.0),
+                new ConstantNode(2.0),
+                new ConstantNode(3.0)
+        );
     }
 
 }
